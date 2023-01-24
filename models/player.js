065d724d46fe57e889cc7c04bd4111a3e84db3b3
kfiddle <kenjfiddle@gmail.com>
2023-01-23 19:41:03 -0500

    email not required now in player model

--- models/player.js
+++ models/player.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const playerSchema = new mongoose.Schema({
   fName: { type: String, required: true },
   lName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: String,
   phone: { type: String, required: true },
   addressLine1: String,
   addressLine2: String,
