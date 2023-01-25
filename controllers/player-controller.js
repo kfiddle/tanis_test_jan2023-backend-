49f4ef5a9f3c9ba4d083d8b92bdd3dd470534c02
kfiddle <kenjfiddle@gmail.com>
2023-01-24 23:12:12 -0500

    players routes and adding in insts

--- controllers/player-controller.js
+++ controllers/player-controller.js
@@ -3,7 +3,6 @@ const Inst = require("../models/inst");
 const HttpError = require("../utils/http-error");
 
 const createPlayer = async (req, res, next) => {
-
   const createdPlayer = new Player({
     fName: req.body.fName,
     lName: req.body.lName,
@@ -15,19 +14,53 @@ const createPlayer = async (req, res, next) => {
     state: req.body.state,
     zip: req.body.zip,
   });
-  
+
   try {
     await createdPlayer.save();
+    console.log(createdPlayer);
   } catch (err) {
-    console.log(err);
     return next(new HttpError("could not create player", 500));
+  }
+
+  let insts = [];
+  for (let instId of req.body.insts) {
+    try {
+      const inst = await Inst.findById(instId);
+      insts.push(inst);
+    } catch (error) {
+      return next(
+        new HttpError("could not locate instrument of id  " + instId, 404)
+      );
+    }
+  }
+
+  for (let inst of insts) {
+    try {
+      if (!inst.players.includes(createdPlayer))
+        inst.players.push(createdPlayer);
+      await inst.save();
+    } catch (error) {
+      return next(
+        new HttpError("could not complete inst adding  " + inst.name, 404)
+      );
+    }
   }
 
   res.status(201).json({ player: createdPlayer.toObject({ getters: true }) });
 };
 
-const getPlayerById = (req, res, next) => {
+const getPlayerById = async (req, res, next) => {
   const playerId = req.params.pid;
+
+  let player;
+
+  try {
+    player = await Player.findById(playerId);
+  } catch (err) {
+    return next(new HttpError("could not find player", 404));
+  }
+
+  res.status(201).json({ player: player.toObject({ getters: true }) });
 };
 
 const getAllPlayers = async (req, res, next) => {
--- routes/player-routes.js
+++ routes/player-routes.js
@@ -4,6 +4,8 @@ const playerController = require("../controllers/player-controller");
 const router = express.Router();
 
 router.get("/", playerController.getAllPlayers);
+router.get("/:pid", playerController.getPlayerById);
+
 router.post("/", playerController.createPlayer);
 router.patch("/add-insts/:pid", playerController.addInstsForPlayer);
 
